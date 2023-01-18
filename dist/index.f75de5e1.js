6d55d6767ddf3436468a8435dded325b67538f73
goescaroline <goes_caroline@hotmail.com>
2023-01-18 16:10:01 -0300

    criando a contagem regressiva

--- dist/index.f75de5e1.js
+++ dist/index.f75de5e1.js
@@ -1,3 +1,22 @@
-console.log("ola mundo");
+AOS.init();
+const dataDoEvento = new Date("Dec 12, 2023 19:00:00");
+const timeStampDoEvento = dataDoEvento.getTime();
+const contaAsHoras = setInterval(function() {
+    const agora = new Date();
+    const timeStampAtual = agora.getTime();
+    const distanciaAteEvento = timeStampDoEvento - timeStampAtual;
+    const diaEmMs = 86400000;
+    const horaEmMs = 3600000;
+    const minutoEmMs = 60000;
+    const diasAteEvento = Math.floor(distanciaAteEvento / diaEmMs);
+    const horasAteEvento = Math.floor(distanciaAteEvento % diaEmMs / horaEmMs);
+    const minutosAteEvento = Math.floor(distanciaAteEvento % horaEmMs / minutoEmMs);
+    const segundosAteEvento = Math.floor(distanciaAteEvento % minutoEmMs / 1000);
+    document.getElementById("contador").innerHTML = `${diasAteEvento}d ${horasAteEvento}h ${minutosAteEvento}m ${segundosAteEvento}s`;
+    if (distanciaAteEvento < 0) {
+        clearInterval(contaAsHoras);
+        document.getElementById("contador").innerHTML = "Evento expirado";
+    }
+}, 1000);
 
 //# sourceMappingURL=index.f75de5e1.js.map
--- dist/scripts/main.js
+++ dist/scripts/main.js
@@ -532,7 +532,26 @@ function hmrAcceptRun(bundle, id) {
 }
 
 },{}],"3cYfC":[function(require,module,exports) {
-console.log("ola mundo");
+AOS.init();
+const dataDoEvento = new Date("Dec 12, 2023 19:00:00");
+const timeStampDoEvento = dataDoEvento.getTime();
+const contaAsHoras = setInterval(function() {
+    const agora = new Date();
+    const timeStampAtual = agora.getTime();
+    const distanciaAteEvento = timeStampDoEvento - timeStampAtual;
+    const diaEmMs = 86400000;
+    const horaEmMs = 3600000;
+    const minutoEmMs = 60000;
+    const diasAteEvento = Math.floor(distanciaAteEvento / diaEmMs);
+    const horasAteEvento = Math.floor(distanciaAteEvento % diaEmMs / horaEmMs);
+    const minutosAteEvento = Math.floor(distanciaAteEvento % horaEmMs / minutoEmMs);
+    const segundosAteEvento = Math.floor(distanciaAteEvento % minutoEmMs / 1000);
+    document.getElementById("contador").innerHTML = `${diasAteEvento}d ${horasAteEvento}h ${minutosAteEvento}m ${segundosAteEvento}s`;
+    if (distanciaAteEvento < 0) {
+        clearInterval(contaAsHoras);
+        document.getElementById("contador").innerHTML = "Evento expirado";
+    }
+}, 1000);
 
 },{}]},["7age3","3cYfC"], "3cYfC", "parcelRequireec61")
 
